3a223aa6033c002637736751214c71191d36125b
aimad <aimad.boulaadam02@gmail.com>
2026-04-24 00:15:10 +0100

    fix: update TEMPLATES DIRS path to use pathlib for search_app templates

--- frontend/config/settings.py
+++ frontend/config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'search_app', 'templates')],
+        'DIRS': [BASE_DIR / 'search_app' / 'templates'],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
