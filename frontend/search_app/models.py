6d04251807f183bfdc722b5056d570659ae821f5
aimad <aimad.boulaadam02@gmail.com>
2026-04-24 18:51:52 +0100

    add SearchHistory model to track user queries and reports

--- frontend/search_app/models.py
+++ frontend/search_app/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import migrations, models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SearchHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    query = models.CharField(max_length=255)
+    report = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at'] # Les plus récents en premier
+
+    def __str__(self):
+        return self.query
