00656fd4ff3166020a7f1dd103b213f445a196c6
aimad <aimad.boulaadam02@gmail.com>
2026-04-24 18:28:08 +0100

    feat: add user authentication routes for registration, login, and logout

--- frontend/search_app/urls.py
+++ frontend/search_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # Page d'accueil
-    path('process/', views.process_query, name='process'), # URL pour l'API
+    path('', views.index, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('process/', views.process_query, name='process'),
 ]
